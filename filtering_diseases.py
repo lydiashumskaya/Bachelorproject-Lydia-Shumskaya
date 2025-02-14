f3b2235b5c371f5f463782f8379b71f4989b3cee
lydiashumskaya <lydia.shumskaya@gmail.com>
2025-02-14 14:41:57 +0100

    Filtering of AML patients

--- filtering_diseases.py
+++ filtering_diseases.py
@@ -2,9 +2,12 @@
 
 import pandas as pd
 
-#loading data about patients 
-df = pd.read_csv("tblpatient.csv")
-diseases = df["disease_simple"]
+#Loading data about patients 
+df_patient = pd.read_csv("tblpatient.csv")
+diseases = df_patient["disease_simple"]
+
+#loading meta data
+df_meta = pd.read_csv("tblmeta_data.csv")
 
 disease_range = {}
 for disease in diseases:
@@ -13,4 +16,13 @@ for disease in diseases:
     else:
         disease_range[disease] = 1
 
-print(disease_range)
+print(disease_range)#Conclusion: 2 groups, AML and others. I will go with AML. 
+
+#Getting patient ID 
+patient_id_with_AML = df_patient[df_patient["disease_simple"] == "AML"]["PatientID"]
+
+#Getting sample ID, day relative to HCT and Accession number of AML patients
+aml_samples = df_meta[df_meta["PatientID"].isin(patient_id_with_AML)][["PatientID", "sampleid", "day_relative_to_hct", "Accession"]]
+
+print(aml_samples)
+
