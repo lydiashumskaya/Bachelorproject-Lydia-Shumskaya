adec3a9b99f5a91bcbb527b0b62689cfa77a1e0f
lydiashumskaya <lydia.shumskaya@gmail.com>
2025-02-21 12:34:43 +0100

    Extracting accession numbers
    NOTE: this also contains 16S RNA

--- filtering_diseases.py
+++ filtering_diseases.py
@@ -24,7 +24,7 @@ patient_id_with_AML = df_patient[df_patient["disease_simple"] == "AML"]["Patient
 #Getting sample ID, day relative to HCT and Accession number of AML patients
 aml_samples = df_meta[df_meta["PatientID"].isin(patient_id_with_AML)][["PatientID", "sampleid", "day_relative_to_hct", "Accession"]]
 
-print(aml_samples)
+#print(aml_samples)
 
 #Making a dictionary with Patient ID and day_relative_to_hct
 relative_days_per_patient ={}
@@ -32,14 +32,32 @@ relative_days_per_patient ={}
 for _, row in aml_samples.iterrows():
     patient = row["PatientID"]
     day_relative = row["day_relative_to_hct"]
+    accession = row["Accession"]
     
     if patient in relative_days_per_patient:
-        relative_days_per_patient[patient].append(day_relative)
+        relative_days_per_patient[patient].append((day_relative, accession))
     else:
-        relative_days_per_patient[patient] = [day_relative]
+        relative_days_per_patient[patient] = [(day_relative, accession)]
         
+#Filtering patients that have only before or only after samples. As well as patients with 1 sample. 
+filtered_relative_days_per_patient = {}
 
-for patient, days in relative_days_per_patient.items():
-    print(f"{patient}: {', '.join(map(str, days))}")
+for patient, data in relative_days_per_patient.items():
+    days = [d[0] for d in data]  # Extract only the days for filtering
+    if len(days) > 1 and any(d < 0 for d in days) and any(d > 0 for d in days):
+        filtered_relative_days_per_patient[patient] = data
 
-#what is the next step 
+# Printing the filtered dictionary
+# for patient, data in filtered_relative_days_per_patient.items():
+#     formatted_data = ", ".join([f"({day}, {accession})" for day, accession in data])
+#     print(f"{patient}: {formatted_data}")
+
+#Filter Accession numbers
+filtered_accessions = {accession for data in filtered_relative_days_per_patient.values() for _, accession in data}
+
+# Save filtered Accession numbers to a text file
+with open("accessions.txt", "w") as f:
+    for accession in filtered_accessions:
+        f.write(accession + "\n")
+
+print("Filtered Accession numbers saved to accessions.txt")
